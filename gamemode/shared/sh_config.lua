b5bd36e43abfe2b8b0ca1e7bf94e3354c8d22bf0
Will <will@miats.com>
2020-07-19 12:53:08 -0700

    Disable prop pickup

--- gamemode/shared/sh_config.lua
+++ gamemode/shared/sh_config.lua
@@ -76,7 +76,7 @@ THIRDPERSON_DISTANCE = 100
 OBJHUNT_TEAM_HUNTERS_CAN_MOVE_PROPS = false
 
 -- this enables TEAM_PROPS to pick up props
-OBJHUNT_TEAM_PROPS_CAN_MOVE_PROPS = true
+OBJHUNT_TEAM_PROPS_CAN_MOVE_PROPS = false
 
 -- entities that are capable of being chosen by props
 USABLE_PROP_ENTITIES = {
