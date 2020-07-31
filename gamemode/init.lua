e9150eb6d234a97ab5ed4eee4d61b2902fd20375
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-07-30 23:02:17 -0700

    Move stuff outside if

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -403,11 +403,11 @@ net.Receive( "Prop Angle Lock", function( len, ply )
 		lockStatus = false
 	end
 
+    local tHitboxMinNonAABB, tHitboxMaxNonAABB = ply:GetProp():GetHitBoxBounds( 0, 0 )
+    local tHitboxMin, tHitboxMax = ply:GetProp():SetAngles(propAngle):GetRotatedAABB(tHitboxMinNonAABB, tHitboxMaxNonAABB)
+    print("NonAABB Hitboxes:", tHitboxMinNonAABB, tHitboxMaxNonAABB, "\n")
+    print("AABB Hitboxes:", tHitboxMin, tHitboxMax, "\n")
 	if (ply.wantPitchEnable) then
-		local tHitboxMinNonAABB, tHitboxMaxNonAABB = ply:GetProp():GetHitBoxBounds( 0, 0 )
-		local tHitboxMin, tHitboxMax = ply:GetProp():SetAngles(propAngle):GetRotatedAABB(tHitboxMinNonAABB, tHitboxMaxNonAABB)
-		print("NonAABB Hitboxes:", tHitboxMinNonAABB, tHitboxMaxNonAABB, "\n")
-		print("AABB Hitboxes:", tHitboxMin, tHitboxMax, "\n")
 		-- we round to reduce getting stuck
 		tHitboxMin = Vector( math.Round(tHitboxMin.x),math.Round(tHitboxMin.y),math.Round(tHitboxMin.z) )
 		tHitboxMax = Vector( math.Round(tHitboxMax.x),math.Round(tHitboxMax.y),math.Round(tHitboxMax.z) )
