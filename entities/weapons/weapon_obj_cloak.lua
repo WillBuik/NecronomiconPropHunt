6f31b6c736cdf298a51cd81f6f30eaa57e01ab6e
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-09-27 20:49:30 -0700

    Fixed cloak

--- entities/weapons/weapon_obj_cloak.lua
+++ entities/weapons/weapon_obj_cloak.lua
@@ -10,13 +10,18 @@ SWEP.AbilityDescription = "Disappear almost completely for $AbilityDuration seco
 function SWEP:Ability()
     local ply = self:GetOwner()
     self:AbilityTimerIfValidOwner(self.AbilityDuration, 1, true, function() self:AbilityCleanup() end )
-    ply:GetProp():SetRenderMode( RENDERMODE_TRANSALPHA )
-    if SERVER then ply:Fire( "alpha", 4, 0 ) end
+    if SERVER then
+        ply:GetProp():SetRenderMode( RENDERMODE_TRANSALPHA )
+        ply:Fire( "alpha", 4, 0 )
+    end
 end
 
 function SWEP:AbilityCleanup()
     if not IsValid( self:GetOwner() ) then return end
     local ply = self:GetOwner()
     ply:GetProp():SetRenderMode( RENDERMODE_NORMAL )
-    if SERVER then ply:Fire( "alpha", 255, 0 ) end
+    if SERVER then
+        ply:GetProp():SetRenderMode( RENDERMODE_NORMAL )
+        ply:Fire( "alpha", 255, 0 )
+    end
 end
