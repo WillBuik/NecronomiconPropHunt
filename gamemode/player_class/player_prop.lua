676b505cc8914d5c8d342b41b5e61212f8065835
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-10-04 20:23:17 -0700

    Ending test

--- gamemode/player_class/player_prop.lua
+++ gamemode/player_class/player_prop.lua
@@ -21,10 +21,9 @@ PLAYER.lastTaunt         = 0.0
 
 function PLAYER:Loadout()
 	self.Player:RemoveAllAmmo()
-    self.Player:Give("weapon_obj_ragdoll")
---     if (math.random(ONE_IN_HOW_MANY_GET_ABILITY) == 1) then
---         self.Player:Give(ABILITIES[ math.random( #ABILITIES ) ])
---     end
+    if (math.random(ONE_IN_HOW_MANY_GET_ABILITY) == 1) then
+        self.Player:Give(ABILITIES[ math.random( #ABILITIES ) ])
+    end
 end
 
 function PLAYER:SetupDataTables()
