7cbfe9023c91cfaeecc299ee8408dfe9c1f8ddbf
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-10-04 21:06:01 -0700

    Fixed errors

--- entities/weapons/weapon_obj_remove.lua
+++ entities/weapons/weapon_obj_remove.lua
@@ -14,3 +14,19 @@ function SWEP:Ability()
     end
 end
 
+local function getViewEnt(ply)
+	-- this needs to be here otherwise some people get errors for some unknown reason
+	if( ply.viewOrigin == nil || ply.wantThirdPerson == nil ) then return end
+
+	local trace = {}
+	trace.mask = MASK_SHOT_HULL
+	trace.start = ply.viewOrigin
+	if( ply.wantThirdPerson ) then
+		trace.endpos = trace.start + ply:GetAngles():Forward() * (THIRDPERSON_DISTANCE+PROP_SELECT_DISTANCE)
+	else
+		trace.endpos = trace.start + ply:GetAngles():Forward() * (PROP_SELECT_DISTANCE)
+	end
+	trace.filter = { ply:GetProp(), ply }
+	tr = util.TraceLine(trace)
+	return tr.Entity
+end
--- gamemode/client/views.lua
+++ gamemode/client/views.lua
@@ -81,6 +81,22 @@ local function stencilColor( ply, ent )
 	return GOOD_HOVER_COLOR
 end
 
+local function getViewEnt(ply)
+	-- this needs to be here otherwise some people get errors for some unknown reason
+	if( ply.viewOrigin == nil || ply.wantThirdPerson == nil ) then return end
+
+	local trace = {}
+	trace.mask = MASK_SHOT_HULL
+	trace.start = ply.viewOrigin
+	if( ply.wantThirdPerson ) then
+		trace.endpos = trace.start + ply:GetAngles():Forward() * (THIRDPERSON_DISTANCE+PROP_SELECT_DISTANCE)
+	else
+		trace.endpos = trace.start + ply:GetAngles():Forward() * (PROP_SELECT_DISTANCE)
+	end
+	trace.filter = { ply:GetProp(), ply }
+	tr = util.TraceLine(trace)
+	return tr.Entity
+end
 
 hook.Add( "PreDrawHalos", "Selectable Prop Halos", function()
 	if( LocalPlayer():Team() != TEAM_PROPS ) then return end
