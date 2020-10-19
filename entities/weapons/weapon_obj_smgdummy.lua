e070be28d98fbd936682ca82c5283e22beed0e4a
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-10-19 02:20:10 -0700

    Fix View model for SMG

--- entities/weapons/weapon_obj_smgdummy.lua
+++ entities/weapons/weapon_obj_smgdummy.lua
@@ -3,6 +3,6 @@ AddCSLuaFile()
 SWEP.Base = "weapon_obj_default"
 
 SWEP.WorldModel = "models/weapons/w_smg1.mdl"
-SWEP.ViewModel = "models/weapons/v_smg1.mdl"
+SWEP.ViewModel = "models/weapons/c_smg1.mdl"
 
 SWEP.HoldType = "smg"
