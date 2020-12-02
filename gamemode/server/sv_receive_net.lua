c63f1d66573b8ac19da4725b848fb0d33858455b
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2020-12-02 12:03:58 -0800

    Forgot a file in the laste commit

--- gamemode/server/sv_receive_net.lua
+++ gamemode/server/sv_receive_net.lua
@@ -18,7 +18,7 @@ hook.Add("Initialize", "Precache all network strings", function()
     util.AddNetworkString("Update Taunt Times")
     util.AddNetworkString("Remove Prop")
     util.AddNetworkString("Prop Roll")
-    util.AddNetworkString("clientpopupopen")
+    util.AddNetworkString("Popup Open")
 end)
 
 net.Receive("Class Selection", function(len, ply)
