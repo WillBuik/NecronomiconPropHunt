f588e804c3ae0438271f86dfb070810e81e1f667
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2021-01-03 00:43:35 -0800

    Chnge hunter loadout to be more fun

--- gamemode/player_class/player_hunter.lua
+++ gamemode/player_class/player_hunter.lua
@@ -19,22 +19,33 @@ PLAYER.UnDuckSpeed       = 0.1
 function PLAYER:Loadout()
 
     self.Player:RemoveAllAmmo()
-    self.Player:GiveAmmo(256, "Pistol", true)
-    self.Player:GiveAmmo(256, "SMG1")
-    self.Player:GiveAmmo(64, "Buckshot")
-    self.Player:GiveAmmo(24, "357")
-    self.Player:GiveAmmo(5, "XBowBolt")
-    self.Player:GiveAmmo(3, "AR2AltFire")
-    self.Player:GiveAmmo(1, "SMG1_Grenade")
     self.Player:Give("weapon_crowbar")
-    self.Player:Give("weapon_hunter_gun_pistol")
-    self.Player:Give("weapon_hunter_gun_smg")
-    self.Player:Give("weapon_hunter_gun_shotgun")
-    -- self.Player:Give("item_ar2_grenade")
-    self.Player:Give("weapon_frag")
-    self.Player:Give("weapon_hunter_gun_revolver")
-    self.Player:Give("weapon_hunter_special_tauntgranade")
-    self.Player:Give("weapon_hunter_special_tauntseeker")
+
+    if (math.random() > 0.5) then
+        self.Player:GiveAmmo(256, "Pistol", true)
+        self.Player:Give("weapon_hunter_gun_pistol")
+    else
+        self.Player:GiveAmmo(36, "357")
+        self.Player:Give("weapon_hunter_gun_revolver")
+    end
+
+    if (math.random() > 0.5) then
+        self.Player:GiveAmmo(256, "SMG1")
+        self.Player:GiveAmmo(1, "SMG1_Grenade")
+        self.Player:Give("weapon_hunter_gun_smg")
+    else
+        self.Player:GiveAmmo(64, "Buckshot")
+        self.Player:Give("weapon_hunter_gun_shotgun")
+    end
+
+    if (math.random() > 0.5) then
+        self.Player:GiveAmmo(5, "XBowBolt")
+        self.Player:Give("weapon_hunter_special_tauntgranade")
+    else
+        self.Player:GiveAmmo(5, "AR2AltFire")
+        self.Player:Give("weapon_hunter_special_tauntseeker")
+    end
+    
     self.Player:Give("weapon_hunter_special_selfdestruct")
 
 
