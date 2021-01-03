e7b2bdfaaa0adbb8e8ec60327b67993487569f41
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2021-01-02 22:54:24 -0800

    selfdestruct

--- entities/weapons/weapon_hunter_special_selfdestruct.lua
+++ entities/weapons/weapon_hunter_special_selfdestruct.lua
@@ -1,114 +1,38 @@
 AddCSLuaFile()
 
-SWEP.Name = "Taunt Grenade"
-SWEP.PrintName = "TAUNT GRENADE"
-SWEP.DrawAmmo = true
-SWEP.DrawCrosshair = true
-SWEP.Slot = 4
-SWEP.SlotPos = 4
+SWEP.Base = "weapon_prop_powerup_base"
+SWEP.Name = "Self-Destruct"
+SWEP.AbilitySound = {"vo/npc/male01/runforyourlife01.wav", "vo/canals/female01/gunboat_farewell.wav", "vo/canals/male01/stn6_incoming.wav"}
+SWEP.AbilityModelScaleTimes = 10
+SWEP.AbilityDuration = math.log(SWEP.AbilityModelScaleTimes)
+SWEP.AbilityDescription = "Creates an explosion after $AbilityDuration seconds that kills nearby props and the user."
 
-SWEP.Weight = 5
-SWEP.AutoSwitchTo = false
-SWEP.AutoSwitchFrom = true
+function SWEP:Ability()
+    if CLIENT then return end
 
-SWEP.HoldType = "grenade"
-SWEP.UseHands = true
-SWEP.ViewModelFOV = 54
-SWEP.ViewModel = "models/weapons/c_bugbait.mdl"
-SWEP.WorldModel = "models/weapons/w_bugbait.mdl"
-
-SWEP.Primary.TakeAmmo = 1
-SWEP.Primary.ClipSize = 1
-SWEP.Primary.Ammo = "XBowBolt"
-SWEP.Primary.DefaultClip = 1
-SWEP.Primary.Automatic = false
-SWEP.Primary.Delay = 1.5
-SWEP.Primary.AutoReload = true
-
-SWEP.AbilityRange = 300
-
-SWEP.Secondary.Ammo = "none"
-
-function SWEP:Initialize()
-    self:SetWeaponHoldType(self.HoldType)
-end
-
-function SWEP:Think()
-end
-
-function SWEP:Throw()
-    if SERVER then
-        local ent = ents.Create("prop_physics")
-        local ply = self:GetOwner()
-        ent:SetModel("models/weapons/w_bugbait.mdl")
-        ent:SetOwner(ply)
-        ent:SetPos(ply:EyePos() + (ply:GetAimVector() * 16))
-        ent:Spawn()
-        ent:SetMaterial("super_bouncy")
-        ent:PhysicsInit(SOLID_VPHYSICS)
-        ent:SetMoveType(MOVETYPE_VPHYSICS)
-        ent:SetSolid(SOLID_BBOX)
-        ent:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
-        util.SpriteTrail(ent, 0, Color(0, 255, 0), false, 16, 16, 0.5, 1 / (15 + 1) * 0.5, "trails/laser.vmt")
-
-        local src = ply:GetShootPos()
-        local target = ply:GetEyeTraceNoCursor().HitPos
-        local tang = (target - src):Angle() -- A target angle to actually throw the grenade to the crosshair instead of fowards
-        -- Makes the grenade go upgwards
-        if tang.p < 90 then
-            tang.p = -10 + tang.p * ((90 + 10) / 90)
-        else
-            tang.p = 360 - tang.p
-            tang.p = -10 + tang.p * -((90 + 10) / 90)
-        end
-        tang.p = math.Clamp(tang.p, -90, 90) -- Makes the grenade not go backwards :/
-        local vel = math.min(800, (90 - tang.p) * 6)
-        local thr = tang:Forward() * vel + ply:GetVelocity()
-
-        local entobj = ent:GetPhysicsObject()
-        entobj:SetVelocity(thr)
-        entobj:AddAngleVelocity(Vector(600, math.random(-1200, 1200), 0))
-
-        timer.Simple(1, function()
-        ent:Ignite(1, 0)
-        end)
-
-        timer.Simple(2, function()
-            local explosion = ents.Create("env_explosion")
-            explosion:SetPos(entobj:GetPos())
-            explosion:SetOwner(ply)
-            explosion:Spawn()
-            explosion:SetKeyValue("iMagnitude", 0)
-            explosion:SetKeyValue("DamageForce", 0)
-            explosion:Fire("Explode", 0, 0)
-            explosion:EmitSound("weapons/bugbait/bugbait_squeeze1.wav", 100, 100)
-
-            local pRange = TAUNT_MAX_PITCH - TAUNT_MIN_PITCH
-            for _, propPlayer in pairs(team.GetPlayers(TEAM_PROPS)) do
-                if (propPlayer:Alive() and propPlayer:GetPos():DistToSqr(entobj:GetPos()) < self.AbilityRange^2) then
-                    local taunt = table.Random(PROP_TAUNTS)
-                    local pitch = math.random() * pRange + TAUNT_MIN_PITCH
-                    SendTaunt(propPlayer, taunt, pitch)
-                end
-            end
-            ent:Remove()
+    for t = self.AbilityModelScaleTimes, 1, -1 do
+        self:AbilityTimerIfValidOwnerAndAlive(math.log(t), 1, true, function()
+            self:GetOwner():SetColor(ColorRand())
+            self:GetOwner():SetPlayerColor(Vector(math.Rand(0, 1), math.Rand(0, 1), math.Rand(0, 1)))
+            self:GetOwner():SetModelScale(self:GetOwner():GetModelScale() + self:GetOwner():GetModelScale() / 10, 0.2)
         end)
     end
-end
 
-function SWEP:PrimaryAttack()
-    if !self:CanPrimaryAttack() then return end
-    timer.Simple(1.5, function()
-        if !self:GetOwner():Alive() or self:GetOwner():GetActiveWeapon():GetClass() != "weapon_obj_tauntgranade" then return end
-        self:Reload()
-        self:SendWeaponAnim(ACT_VM_DRAW)
+    self:AbilityTimerIfValidOwnerAndAlive(self.AbilityDuration, 1, true, function()
+        local explode = ents.Create("env_explosion")
+        explode:SetPos(self:GetOwner():GetPos())
+        explode:SetOwner(self:GetOwner())
+        explode:Spawn()
+        explode:SetKeyValue("iMagnitude", "112")
+        explode:Fire( "Explode", 0, 0 )
+        explode:EmitSound( "BaseExplosionEffect.Sound", 100, 100 )
     end)
-    self:Throw()
-    self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-    self:TakePrimaryAmmo(self.Primary.TakeAmmo)
-    self:GetOwner():DoAttackEvent()
-    self:SendWeaponAnim(ACT_VM_THROW)
 end
 
-function SWEP:SecondaryAttack()
+function SWEP:AbilityCleanup()
+    if IsValid(self:GetOwner()) then
+        self:GetOwner():Kill()
+        self:GetOwner():SetModelScale(1, 0.1)
+        self:GetOwner():SetColor(Color( 255, 255, 255, 255))
+    end
 end
