65c3aff3a294ae86bea1dc41f7555bd716bf49b8
Calvin Loncaric <marvinx03@gmail.com>
2021-01-10 14:37:59 -0800

    Update taunt delay on every taunt, regardless of source
    I am not sure if this is what @kvovin had in mind, but it closes a
    "mini-exploit":
    
      - Round starts, player is assigned a very long NextAutoTauntDelay to
        account for hide time
      - Time passes, hunters released
      - Player taunts manually, updating LastTauntTime but *not*
        NextAutoTauntDelay
      - Player now has a *very* long time before their next assigned
        auto-taunt >:)
      - Player keeps taunting manually to avoid ever being assigned a
        shorter NextAutoTauntDelay >:) >:) >:)

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -105,9 +105,18 @@ function SendTaunt(ply, taunt, pitch)
 
     print("Taunt: " .. taunt .. " from " .. ply:Nick() .. " at pitch " .. tostring(pitch) .. "; pre-pitch-adjusted duration is " .. duration .. "s" )
 
+    local adjustedDuration = duration * (100 / pitch)
+
     ply:SetLastTauntTime(CurTime())
-    ply:SetLastTauntDuration(duration * (100 / pitch))
+    ply:SetLastTauntDuration(adjustedDuration)
     ply:SetLastTauntPitch(pitch)
+
+    -- NOTE: +1 on the modifier to ensure that the previous taunt doesn't count
+    -- against the player's time, even if the modifier is 0.
+    ply:SetNextAutoTauntDelay(
+        (OBJHUNT_AUTOTAUNT_DURATION_MODIFIER + 1) * adjustedDuration +
+        OBJHUNT_AUTOTAUNT_BASE_INTERVAL
+    )
 
     net.Start("Taunt Selection BROADCAST")
         net.WriteString(taunt)
