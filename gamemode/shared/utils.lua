ca89dcabee7808ceae0c59105e7693618c3843e6
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2021-02-22 14:32:29 -0800

    real ID

--- gamemode/shared/utils.lua
+++ gamemode/shared/utils.lua
@@ -288,7 +288,7 @@ end
 function PlayerToAccentColor(ply)
     if !ply:AccountID() then return Vector(0,0,0) end
     local cubeID = ply:AccountID() ^ 3
-    print(ply:AccountID())
+    print(ply:Steam())
     return Vector(
         ((cubeID * 3) % 256) / 255.0,
         ((cubeID * 5) % 256) / 255.0,
