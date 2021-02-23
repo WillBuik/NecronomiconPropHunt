13d4049feaac8436eadebdd9dfaa133d2cea9c4f
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2021-02-22 16:26:05 -0800

    Hopefully refactor bongcloud into something useable

---
+++ entities/effects/ph_bongcloud.lua
@@ -0,0 +1,42 @@
+function EFFECT:Init(data)
+    self:SetOwner(data:GetEntity())
+    local radius = data:GetRadius()
+    local fadeoutTime = data:GetScale()
+    local em = ParticleEmitter(self:GetOwner():GetPos())
+
+        local smokeparticles = {
+            Model("particle/particle_smokegrenade"),
+            Model("particle/particle_noisesphere")
+         } 
+        
+
+        for i = 1, radius do
+            local prpos = VectorRand() * radius
+            prpos.z = prpos.z + 32
+            local p = em:Add(table.Random(smokeparticles), center + prpos)
+            if p then
+                local gray = math.random(75, 200)
+                p:SetColor(gray, gray, gray)
+                p:SetStartAlpha(255)
+                p:SetEndAlpha(200)
+                p:SetVelocity(VectorRand() * math.Rand(900, 1300))
+                p:SetLifeTime(0)
+                
+                p:SetDieTime(math.Rand(math.max(0, fadeoutTime - 20), fadeoutTime))
+
+                p:SetStartSize(math.random(140, 150))
+                p:SetEndSize(math.random(1, 40))
+                p:SetRoll(math.random(-180, 180))
+                p:SetRollDelta(math.Rand(-0.1, 0.1))
+                p:SetAirResistance(600)
+
+                p:SetCollide(true)
+                p:SetBounce(0.4)
+
+                p:SetLighting(false)
+            end
+        end
+
+        em:Finish()
+    end)
+end
--- entities/weapons/weapon_prop_powerup_bongcloud.lua
+++ entities/weapons/weapon_prop_powerup_bongcloud.lua
@@ -17,54 +17,10 @@ function SWEP:Ability()
 end
 
 function SWEP:CreateSmoke()
-    for _,v in pairs(player.GetAll()) do
-        net.Start("Bong Smokecloud")
-            net.WriteVector(self:GetOwner():GetPos())
-            net.ReadUInt(self.AbilityRadius, 16)
-        net.Send(v)
-    end
-end
-
-if CLIENT then
-    net.Receive("Bong Smokecloud", function(len , ply)
-        local center = net.ReadVector()
-        local radius = net.ReadUInt(16)
-        local em = ParticleEmitter(center)
-
-        local smokeparticles = {
-            Model("particle/particle_smokegrenade"),
-            Model("particle/particle_noisesphere")
-         } 
-        
-
-        for i = 1, radius do
-            local prpos = VectorRand() * radius
-            prpos.z = prpos.z + 32
-            local p = em:Add(table.Random(smokeparticles), center + prpos)
-            if p then
-                local gray = math.random(75, 200)
-                p:SetColor(gray, gray, gray)
-                p:SetStartAlpha(255)
-                p:SetEndAlpha(200)
-                p:SetVelocity(VectorRand() * math.Rand(900, 1300))
-                p:SetLifeTime(0)
-                
-                p:SetDieTime(math.Rand(50, 70))
-
-                p:SetStartSize(math.random(140, 150))
-                p:SetEndSize(math.random(1, 40))
-                p:SetRoll(math.random(-180, 180))
-                p:SetRollDelta(math.Rand(-0.1, 0.1))
-                p:SetAirResistance(600)
-
-                p:SetCollide(true)
-                p:SetBounce(0.4)
-
-                p:SetLighting(false)
-            end
-        end
-
-        em:Finish()
-    end)
+        local effect = EffectData()
+        effect:SetEntity(self:GetOwner())
+        effect.SetRadius(self.AbilityRadius)
+        effect:SetScale(self.AbilityDuration)
+        util.Effect("ph_blastoff", effect, true, true)
 end
 
