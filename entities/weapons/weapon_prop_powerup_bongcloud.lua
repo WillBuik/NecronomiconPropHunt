6a6c8b4449a4a468ed355e04d451bff3ed694433
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2021-04-25 12:56:22 -0700

    CLean up bonglcoud code

--- entities/weapons/weapon_prop_powerup_bongcloud.lua
+++ entities/weapons/weapon_prop_powerup_bongcloud.lua
@@ -11,36 +11,40 @@ SWEP.AbilityDescription = "Create clouds of smoke at your location for $AbilityD
 
 function SWEP:Ability()
     if CLIENT then return end
-    self:CreateSmoke()
+    self:CreateAllSmoke()
     local originalCenter = self:GetOwner():GetPos()
-    self:AbilityTimerIfValidOwner(self.AbilityDuration / (self.AbilityEmits - 1), self.AbilityEmits - 1, true, function() self:CreateSmoke(originalCenter) end)
+    self:AbilityTimerIfValidOwner(self.AbilityDuration / (self.AbilityEmits - 1), self.AbilityEmits - 1, true, function() self:CreateAllSmoke(originalCenter) end)
 end
 
-function SWEP:CreateSmoke(originalCenter)
+function SWEP:CreateAllSmoke(originalCenter)
+        local othersFilter = RecipientFilter()
+        othersFilter:AddAllPlayers()
+        othersFilter:RemovePlayer(self:GetOwner())
+        self:CreateSomeSmoke(originalCenter, 255, othersFilter)
+
+        local ownerFilter = RecipientFilter()
+        ownerFilter:AddPlayer(self:GetOwner())
+        self:CreateSomeSmoke(originalCenter, 100, ownerFilter)
+end
+
+function SWEP:CreateSomeSmoke(originalCenter, alpha, recipientFilter)
         local currentCenter = self:GetOwner():GetPos()
         local effect = EffectData()
         effect:SetEntity(self:GetOwner())
         effect:SetOrigin(currentCenter)
         effect:SetRadius(self.AbilityRadius)
         effect:SetScale(self.AbilityDuration * 3)
-        effect:SetMagnitude(255)
-        local othersFilter = RecipientFilter()
-        othersFilter:AddAllPlayers()
-        othersFilter:RemovePlayer(self:GetOwner())
-        util.Effect("ph_bongcloud", effect, true, othersFilter)
-        effect:SetMagnitude(55)
-        local ownerFilter = RecipientFilter()
-        ownerFilter:AddPlayer(self:GetOwner())
-        util.Effect("ph_bongcloud", effect, true, ownerFilter)
+        effect:SetMagnitude(alpha)
+        util.Effect("ph_bongcloud", effect, true, recipientFilter)
         if !originalCenter then return end
         local differanceVec =
             Vector(currentCenter.x - originalCenter.x , currentCenter.y - originalCenter.y, 0)
         if differanceVec:IsZero() then return end
 
         effect:SetOrigin(AddAngleToXY(differanceVec, 2 * math.pi / 3) + originalCenter)
-        util.Effect("ph_bongcloud", effect, true, true)
+        util.Effect("ph_bongcloud", effect, true, recipientFilter)
 
         effect:SetOrigin(AddAngleToXY(differanceVec, -2 * math.pi / 3) + originalCenter)
-        util.Effect("ph_bongcloud", effect, true, true)
+        util.Effect("ph_bongcloud", effect, true, recipientFilter)
 end
 
