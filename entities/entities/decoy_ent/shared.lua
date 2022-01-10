27041aecd8d49a05cdd6d320ca5ec90e2cb9238d
Kevin Varela-O'Hara <kev.v.oh@gmail.com>
2022-01-09 20:23:02 -0800

    Got rid of complexities

--- entities/entities/decoy_ent/shared.lua
+++ entities/entities/decoy_ent/shared.lua
@@ -6,7 +6,7 @@ ENT.Type = "nextbot"
 function ENT:Initialize()
     self:SetModel( "models/seagull.mdl" )
     self:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
-    self.IsRunning = true
+    --self.IsRunning = true
     self.FearRadius = 1000
 end
 
@@ -22,12 +22,12 @@ end
 
 function ENT:RunBehavior()
     while (true) do
-        if (self.IsRunning and self:PlayerNear()) then
+        if (self:PlayerNear()) then
             self.loco:SetDesiredSpeed(300)
             self:StartActivity(ACT_RUN)
             self:MoveToPos(self:GetPos() + Vector(math.random(-1, 1), math.random(-1, 1), 0) * self.FearRadius)
         else
-            self.IsRunning = false
+            --self.IsRunning = false
             self:StartActivity(ACT_IDLE)
         end
         coroutine.wait(1)
