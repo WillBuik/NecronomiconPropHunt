5f335d5b6ce8e0e25f214ddddc09a4b2e0bf52d0
Will Buik <will@miats.com>
2024-01-10 06:54:03 +0000

    Fix: move disguise to server side to hide player prop

--- entities/weapons/weapon_prop_powerup_disguise.lua
+++ entities/weapons/weapon_prop_powerup_disguise.lua
@@ -7,38 +7,38 @@ SWEP.PrintName = "Disguise"
 SWEP.AbilityDescription = "Transforms you into a random hunter until you turn it off."
 
 function SWEP:Ability()
-    local ply = self:GetOwner()
+    if CLIENT then return end
 
+    local ply = self:GetOwner()
     ply:ObjSetDisguised(true)
     ply:SetModel(TEAM_HUNTERS_DEFAULT_MODEL)
     ply:SetRenderMode(RENDERMODE_NORMAL)
     ply:GetProp():SetRenderMode(RENDERMODE_NONE)
-    if SERVER then
-        ply:Give("weapon_prop_util_smgdummy")
-        ply:SelectWeapon("weapon_prop_util_smgdummy")
 
-        ply:ResetHull()
-        local tHitboxMin, tHitboxMax = ply:GetHull()
-        UpdatePlayerPropHitbox(ply, tHitboxMin, tHitboxMax)
-        local foundSpot = FindSpotFor(ply, tHitboxMin, tHitboxMax)
-        ply:SetPos(foundSpot)
-        net.Start("Prop Update")
-            net.WriteVector(tHitboxMax)
-            net.WriteVector(tHitboxMin)
-        net.Send(ply)
-    end
+    ply:Give("weapon_prop_util_smgdummy")
+    ply:SelectWeapon("weapon_prop_util_smgdummy")
+
+    ply:ResetHull()
+    local tHitboxMin, tHitboxMax = ply:GetHull()
+    UpdatePlayerPropHitbox(ply, tHitboxMin, tHitboxMax)
+    local foundSpot = FindSpotFor(ply, tHitboxMin, tHitboxMax)
+    ply:SetPos(foundSpot)
+    net.Start("Prop Update")
+        net.WriteVector(tHitboxMax)
+        net.WriteVector(tHitboxMin)
+    net.Send(ply)
 end
 
 function SWEP:AbilityCleanup()
+    if CLIENT then return end
+
     if !IsValid(self:GetOwner()) then return end
     local ply = self:GetOwner()
     ply:ObjSetDisguised(false)
-    if SERVER then
-        player_manager.RunClass(ply, "SetModel")
-        if (IsValid(ply:GetProp())) then
-            ply:GetProp():SetRenderMode(RENDERMODE_NORMAL)
-            ResetPropToProp(ply)
-        end
-        ply:SetRenderMode(RENDERMODE_NONE)
+    player_manager.RunClass(ply, "SetModel")
+    if (IsValid(ply:GetProp())) then
+        ply:GetProp():SetRenderMode(RENDERMODE_NORMAL)
+        ResetPropToProp(ply)
     end
+    ply:SetRenderMode(RENDERMODE_NONE)
 end
